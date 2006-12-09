a727b785c879f89d5b1254987061848a2c3b0752
till <till>
2006-12-09 17:08:40 +0000

    Serializable. Added a generated serialVersionUID to suppress warning.
    Added comments.

--- src/com/aetrion/flickr/auth/Permission.java
+++ src/com/aetrion/flickr/auth/Permission.java
@@ -15,14 +15,54 @@ import java.util.Map;
  */
 public class Permission implements Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5384461370301078353L;
+	/**
+	 * Type value for no permissions
+	 * If no permissions are requested, the application will only display public photos.
+	 */
     public static final int NONE_TYPE = 0;
+    /**
+     * Type value for Read Permission.
+	 * You will be able to see all your photos via the application. 
+	 * (This includes your private photos.)
+     */
     public static final int READ_TYPE = 1;
+    /**
+     * Type value for Write Permission (and Read). 
+     * You will be able to see all your photos, upload new photos, 
+     * and add, edit or delete photo metadata (titles, descriptions, tags, etc.).
+     */
     public static final int WRITE_TYPE = 2;
+    /**
+     * Type value for Delete Permission (and Write, Read).
+     * You are able to delete Flickr photos via the application.
+     */
     public static final int DELETE_TYPE = 3;
 
+	/**
+	 * No permissions.
+	 * If no permissions are requested, the application will only display public photos.
+	 */
     public static final Permission NONE = new Permission(NONE_TYPE);
+    /**
+     * Read Permission.
+	 * You will be able to see all your photos via the application. 
+	 * (This includes your private photos.)
+     */
     public static final Permission READ = new Permission(READ_TYPE);
+    /**
+     * Write (and Read).
+     * You will be able to see all your photos, upload new photos, 
+     * and add, edit or delete photo metadata (titles, descriptions, tags, etc.).
+     */
     public static final Permission WRITE = new Permission(WRITE_TYPE);
+    /**
+     * Delete (and Write, Read).
+     * You are able to delete Flickr photos via the application.
+     */
     public static final Permission DELETE = new Permission(DELETE_TYPE);
 
     private static final Map stringToPermissionMap = new HashMap();
