35ccc478bba9fcdf568e6ab0bc09b447f062082b
till <till>
2006-12-09 17:14:15 +0000

    added Throttle

--- src/com/aetrion/flickr/groups/GroupsInterface.java
+++ src/com/aetrion/flickr/groups/GroupsInterface.java
@@ -25,6 +25,9 @@ import org.xml.sax.SAXException;
 public class GroupsInterface {
 
     public static final String METHOD_BROWSE = "flickr.groups.browse";
+    /**
+     * @deprecated
+     */
     public static final String METHOD_GET_ACTIVE_LIST = "flickr.groups.getActiveList";
     public static final String METHOD_GET_INFO = "flickr.groups.getInfo";
     public static final String METHOD_SEARCH = "flickr.groups.search";
@@ -158,31 +161,51 @@ public class GroupsInterface {
         Element groupElement = response.getPayload();
         Group group = new Group();
         group.setId(groupElement.getAttribute("id"));
-
+        group.setIconServer(groupElement.getAttribute("iconserver"));
         group.setName(XMLUtilities.getChildValue(groupElement, "name"));
+        group.setDescription(XMLUtilities.getChildValue(groupElement, "description"));
         group.setMembers(XMLUtilities.getChildValue(groupElement, "members"));
-        group.setOnline(XMLUtilities.getChildValue(groupElement, "online"));
         group.setPrivacy(XMLUtilities.getChildValue(groupElement, "privacy"));
+        
+        NodeList throttleNodes = groupElement.getElementsByTagName("throttle");
+        int n = throttleNodes.getLength();
+        if (n == 1) {
+        	Element throttleElement = (Element) throttleNodes.item(0);
+        	Throttle throttle = new Throttle();
+        	group.setThrottle(throttle);
+        	throttle.setMode(throttleElement.getAttribute("mode"));
+        	String countStr = throttleElement.getAttribute("count");
+        	String remainingStr = throttleElement.getAttribute("remaining");
+        	if (countStr != null && countStr.length() > 0) {
+        		throttle.setCount(Integer.parseInt(countStr));
+        	}
+        	if (remainingStr != null && remainingStr.length() > 0) {
+        		throttle.setRemaining(Integer.parseInt(remainingStr));
+        	}
+        } else if (n > 1) {
+        	System.err.println("WARNING: more than one throttle element in group");
+        }
+        // the following are not delivered anymore (?) but stay here in case ....
+        group.setOnline(XMLUtilities.getChildValue(groupElement, "online"));
         group.setChatId(XMLUtilities.getChildValue(groupElement, "chatid"));
         group.setInChat(XMLUtilities.getChildValue(groupElement, "chatcount"));
 
         return group;
     }
-
+    
     /**
-     * Search for groups.
-     *
-     * @param text The text
-     * @param perPage The number of groups per page
-     * @param page The number of pages
-     * @return A collection of Group objects
-     * @throws IOException A collection of Group objects
+     * Search for groups. 18+ groups will only be returned for authenticated calls where the authenticated user is over 18.
+     * This method does not require authentication. 
+     * @param text The text to search for.
+     * @param perPage Number of groups to return per page. If this argument is 0, it defaults to 100. The maximum allowed value is 500.
+     * @param page The page of results to return. If this argument is 0, it defaults to 1.
+     * @return A GroupList Object. Only the fields <em>id</em>, <em>name</em> and <em>eighteenplus</em> in the Groups will be set.
+     * @throws IOException 
      * @throws SAXException
      * @throws FlickrException
      */
-    public Collection search(String text, int perPage, int page) throws IOException, SAXException, FlickrException {
-        List groups = new ArrayList();
-
+    public Collection search(String text, int perPage, int page) throws FlickrException, IOException, SAXException {
+    	GroupList groupList = new GroupList();
         List parameters = new ArrayList();
         parameters.add(new Parameter("method", METHOD_SEARCH));
         parameters.add(new Parameter("api_key", apiKey));
@@ -202,14 +225,18 @@ public class GroupsInterface {
         }
         Element groupsElement = response.getPayload();
         NodeList groupNodes = groupsElement.getElementsByTagName("group");
+        groupList.setPage(XMLUtilities.getIntAttribute(groupsElement, "page"));
+        groupList.setPages(XMLUtilities.getIntAttribute(groupsElement, "pages"));
+        groupList.setPerPage(XMLUtilities.getIntAttribute(groupsElement, "perpage"));
+        groupList.setTotal(XMLUtilities.getIntAttribute(groupsElement, "total"));
         for (int i = 0; i < groupNodes.getLength(); i++) {
             Element groupElement = (Element) groupNodes.item(i);
             Group group = new Group();
             group.setId(groupElement.getAttribute("nsid"));
             group.setName(groupElement.getAttribute("name"));
-            group.setEighteenPlus("1".equals(groupElement.getAttribute("eighteenplus")));
-            groups.add(group);
-        }
-        return groups;
+            group.setEighteenPlus(XMLUtilities.getBooleanAttribute(groupElement, "eighteenplus"));
+            groupList.add(group);
+        }    	
+		return groupList;
     }
 }
