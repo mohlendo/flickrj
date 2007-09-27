f883e1449715cc76ece4778b3775c1774ae66de4
x-mago <x-mago>
2007-09-27 19:21:54 +0000

    doc update

--- src/com/aetrion/flickr/photos/Extras.java
+++ src/com/aetrion/flickr/photos/Extras.java
@@ -1,18 +1,18 @@
 /*
  * Copyright (c) 2005 Aetrion LLC.
  */
-
 package com.aetrion.flickr.photos;
 
 import java.util.HashSet;
 import java.util.Set;
 
 /**
+ * Extra-attributes for Photo-requests.
+ * 
  * @author Anthony Eden
- * @version $Id: Extras.java,v 1.4 2007/09/15 23:00:28 x-mago Exp $
+ * @version $Id: Extras.java,v 1.5 2007/09/27 19:21:54 x-mago Exp $
  */
 public class Extras {
-
     public static final String KEY_EXTRAS = "extras";
 
     public static final String LICENSE = "license";
@@ -36,6 +36,7 @@ public class Extras {
      * @see com.aetrion.flickr.photos.PhotosInterface#getWithGeoData(java.util.Date, java.util.Date, java.util.Date, java.util.Date, int, String, Set, int, int)
      * @see com.aetrion.flickr.photos.PhotosInterface#getWithoutGeoData(java.util.Date, java.util.Date, java.util.Date, java.util.Date, int, String, Set, int, int)
      * @see com.aetrion.flickr.photos.PhotosInterface#recentlyUpdated(java.util.Date, Set, int, int)
+     * @see com.aetrion.flickr.interestingness.InterestingnessInterface#getList(java.util.Date, Set, int, int)
      */
     public static final Set ALL_EXTRAS = new HashSet();
 
@@ -43,10 +44,14 @@ public class Extras {
      * Minimal Set of extra-arguments. Used by convenience-methods
      * that request lists of photos.
      *
-     * @see com.aetrion.flickr.people.PeopleInterface#getPublicPhotos(String, int, int)
-     * @see com.aetrion.flickr.groups.pools.PoolsInterface#getPhotos(String, String[], int, int)
-     * @see com.aetrion.flickr.photosets.PhotosetsInterface#getPhotos(String, int, int)
-     * @see com.aetrion.flickr.photos.PhotosInterface#getContactsPublicPhotos(String, int, boolean, boolean, boolean)
+     * @see com.aetrion.flickr.people.PeopleInterface#getPublicPhotos(String, Set, int, int)
+     * @see com.aetrion.flickr.groups.pools.PoolsInterface#getPhotos(String, String[], Set, int, int)
+     * @see com.aetrion.flickr.photosets.PhotosetsInterface#getPhotos(String, Set, int, int, int)
+     * @see com.aetrion.flickr.photos.PhotosInterface#getContactsPublicPhotos(String, Set, int, boolean, boolean, boolean)
+     * @see com.aetrion.flickr.photos.PhotosInterface#getWithGeoData(java.util.Date, java.util.Date, java.util.Date, java.util.Date, int, String, Set, int, int)
+     * @see com.aetrion.flickr.photos.PhotosInterface#getWithoutGeoData(java.util.Date, java.util.Date, java.util.Date, java.util.Date, int, String, Set, int, int)
+     * @see com.aetrion.flickr.photos.PhotosInterface#recentlyUpdated(java.util.Date, Set, int, int)
+     * @see com.aetrion.flickr.interestingness.InterestingnessInterface#getList(java.util.Date, Set, int, int)
      */
     public static final Set MIN_EXTRAS = new HashSet();
 
